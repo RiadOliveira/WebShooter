3148601845693d7f7ff7d0b735ddf9cdfb3a9a5b
Ríad Oliveira <riad.oliveira@hotmail.com>
2024-12-27 21:59:11 -0300

    Line break added to exitWithError Message

--- src/utils/error.c
+++ src/utils/error.c
@@ -5,6 +5,7 @@ inline void exitWithError(const char* format, ...) {
 
   va_start(messages, format);
   vfprintf(stderr, format, messages);
+  fprintf(stderr, "\n");
   va_end(messages);
 
   exit(EXIT_FAILURE);
